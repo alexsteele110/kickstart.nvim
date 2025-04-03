d19adcc8f5f5c8b90e522744cd1a3ac218623af4
Alex Steele <alexsteele110@gmail.com>
2025-04-03 16:04:56 -0400

    add git keybindings

--- init.lua
+++ init.lua
@@ -267,6 +267,47 @@ require('lazy').setup({
         topdelete = { text = '‾' },
         changedelete = { text = '~' },
       },
+      on_attach = function(bufnr)
+        local gs = package.loaded.gitsigns
+
+        local function map(mode, lhs, rhs, desc)
+          vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc })
+        end
+
+        -- Navigation
+        map('n', ']c', function()
+          if vim.wo.diff then
+            return ']c'
+          end
+          vim.schedule(gs.next_hunk)
+          return '<Ignore>'
+        end, 'Next Hunk')
+
+        map('n', '[c', function()
+          if vim.wo.diff then
+            return '[c'
+          end
+          vim.schedule(gs.prev_hunk)
+          return '<Ignore>'
+        end, 'Prev Hunk')
+
+        -- Actions
+        map({ 'n', 'v' }, '<leader>hs', ':Gitsigns stage_hunk<CR>', 'Stage Hunk')
+        map({ 'n', 'v' }, '<leader>hr', ':Gitsigns reset_hunk<CR>', 'Reset Hunk')
+        map('n', '<leader>hS', gs.stage_buffer, 'Stage Buffer')
+        map('n', '<leader>hu', gs.undo_stage_hunk, 'Undo Stage Hunk')
+        map('n', '<leader>hR', gs.reset_buffer, 'Reset Buffer')
+        map('n', '<leader>hp', gs.preview_hunk, 'Preview Hunk')
+        map('n', '<leader>hb', function()
+          gs.blame_line { full = true }
+        end, 'Blame Line')
+        map('n', '<leader>tb', gs.toggle_current_line_blame, 'Toggle Line Blame')
+        map('n', '<leader>hd', gs.diffthis, 'Diff This')
+        map('n', '<leader>hD', function()
+          gs.diffthis '~'
+        end, 'Diff This ~')
+        map('n', '<leader>td', gs.toggle_deleted, 'Toggle Deleted')
+      end,
     },
   },
 
